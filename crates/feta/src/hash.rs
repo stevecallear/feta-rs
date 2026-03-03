9237b94ee9b8d059940a2ba5d3b1a1e24e7bdaa7
Steve Callear <scallear@gmail.com>
2026-03-03 09:54:19 +0000

    refactor: remove expect for unreachable code

--- crates/feta/src/hash.rs
+++ crates/feta/src/hash.rs
@@ -7,5 +7,5 @@ pub fn calculate(feature: &str, user_key: &str) -> u32 {
     key.push_str(user_key);
 
     // there are no error paths for Cursor::read, so we can assume this will succeed
-    murmur3::murmur3_32(&mut Cursor::new(&key), 0).expect("failed to calculate hash")
+    murmur3::murmur3_32(&mut Cursor::new(&key), 0).unwrap()
 }
